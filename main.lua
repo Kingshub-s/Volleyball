5265686177e4bc9cc1b9b859db36e32c76aafc70
Kingshub-s <zakikdj8@gmail.com>
2026-07-17 18:00:05 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -36,7 +36,6 @@ local Toggles = Library.Toggles
 Library.ForceCheckbox = false
 Library.ShowToggleFrameInKeybinds = true
 Library.ShowCustomCursor = false
--- Library:SetNotifySide(...)
 
 local function notify(message, lifetime)
     Library:Notify({
