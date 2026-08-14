28741dd488674ba81b3a9ea841aa607a45b4df9b
Kingshub-s <zakikdj8@gmail.com>
2026-08-14 18:39:16 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/1afd6145f25057bdeb244f86d6affd20dfb10e2866512e161019f1acbe1ccc3c/download"))()
+loadstring(game:HttpGet("https://vss.pandauth.com/kv/84aa4db204200073"))()
